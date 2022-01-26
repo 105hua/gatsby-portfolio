c8290f846cdf7fd13fbcfc278b824e0367e099fc
105hua <joshuajtudball2003@gmail.com>
2022-01-26 22:06:21 +0000

    Started on the 'What I'm currently doing' section

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -13,6 +13,12 @@ const buttonGroupStyle = {
 
 }
 
+const currentlyDoingStyle = {
+
+    textAlign: "right",
+
+}
+
 const separator = {
 
     marginTop: "75px",
@@ -44,7 +50,7 @@ const AboutMePage = () => {
 
                 <TypeWriter onInit={ (typewriter) => {
 
-                    typewriter.typeString("Early Beginnings...").start();
+                    typewriter.typeString("How I Got Started.").start();
 
                 } } ></TypeWriter>
 
@@ -58,15 +64,41 @@ const AboutMePage = () => {
                         of my Primary School years, I would continue to tinker with computers, making a<br />
                         handful of basic programs and games in the process.<br /><br />
 
-                        In my transition to and during Secondary School, I began learning my first High-Level<br />
-                        Programming Language, Python 3. I learned this programming language as part of my<br />
-                        GSCE Computer Science course, during which I took on my first Programming Project.<br />
-                        The project in question required me to create a Register and Login screen, which would<br />
-                        store its data inside of a CSV File. 
+                        In my transition to and during Secondary School in 2014, I began learning my first High-Level<br />
+                        High-Level Programming Language, Python 3. I learned this programming language as<br />
+                        part of my GSCE Computer Science course. After graduating Secondary School, I moved<br />
+                        on to do an A-Level in Computer Science and Games Development. During this course, I<br />
+                        learned two new programming languages, these were C# and Visual Basic, as an<br />
+                        understanding of the two languages were required for the course. After graduating College,<br />
+                        I decided to enter University and study a Baccalaureate in Computer Science, which I am<br />
+                        currently still studying.
 
                     </p>
 
                 </FadeIn>
+
+            </div>
+
+            <div style={separator}>
+
+                <div style={currentlyDoingStyle} id="currentlydoing">
+
+                    <TypeWriter onInit={ (typewriter) => {
+
+                        typewriter.typeString("What I am currently doing.").start();
+
+                    } }></TypeWriter>
+
+                    <p class="alignright">
+
+                        Nowadays, I am trying to stick to three programming languages, those being C#, LUA and React.<br />
+                        C# allows me to create efficient and easy to obtain Desktop Applications, LUA allows me to work<br />
+                        with the popular online game platform, Roblox and React allows me to create beautiful and responsive<br />
+                        websites.
+
+                    </p>
+
+                </div>
 
             </div>
 
